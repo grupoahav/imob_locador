3f10197640346742ce5500f387b7dc6090d852d3
Marcos Souza <devmrcs@gmail.com>
2021-08-17 02:08:38 -0300

    🚚 rename to right success

--- packages/imob_core/lib/src/domain/enums/control_state_enum.dart
+++ packages/imob_core/lib/src/domain/enums/control_state_enum.dart
@@ -4,7 +4,7 @@ enum ControlState {
   start,
   empty,
   loading,
-  sucess,
+  success,
   failure,
 }
 
@@ -14,11 +14,11 @@ extension ParseToString on ControlState {
   bool get isStart => this == ControlState.start;
   bool get isEmpty => this == ControlState.empty;
   bool get isLoading => this == ControlState.loading;
-  bool get isSucess => this == ControlState.sucess;
+  bool get isSuccess => this == ControlState.success;
   bool get isFailure => this == ControlState.failure;
   bool get isNotStart => this != ControlState.start;
   bool get isNotEmpty => this != ControlState.empty;
   bool get isNotLoading => this != ControlState.loading;
-  bool get isNotSucess => this != ControlState.sucess;
+  bool get isNotSuccess => this != ControlState.success;
   bool get isNotFailure => this != ControlState.failure;
 }
